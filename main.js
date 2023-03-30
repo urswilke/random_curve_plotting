4058f522b1fbf242eafe92623e8ad4b7906c21b6
urswilke <urs.wilke@gmail.com>
2023-03-30 14:41:11 +0200

    also update plot when button is clicked

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 import plotLines from "./plot.js";
 import * as RndNotes from "./randomNotes.js";
 var data = RndNotes.gen_mult_arrays_flat(10, 30);
-var scaleNotes = RndNotes.getChecked();
-var allScaleNotes = RndNotes.getAllScaleNotes(scaleNotes);
 
-plotLines(document.body, data, allScaleNotes);
+function adaptToSelectedNotes() {
+    var scaleNotes = RndNotes.getChecked();
+    var allScaleNotes = RndNotes.getAllScaleNotes(scaleNotes);
+    RndNotes.getChecked();
+    plotLines(document.body, data, allScaleNotes);
+}
 
-document.getElementById("get-notes-button").addEventListener('click', RndNotes.getChecked);
+
+
+document.getElementById("get-notes-button").addEventListener('click', adaptToSelectedNotes);
